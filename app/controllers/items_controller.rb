04cbef0eef135ee2509dc1abaabd2f9eb53ef58b
Agnes <agsawicka@gmail.com>
2022-09-06 13:49:14 -0400

    commit tree 00140829cf3e15bf7e3b1182cde6767cd20bc2d4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,46 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user 
+      items = user.items
+      else 
+        return render json: {error: "404 Not Found"}, status: :not_found
+      end
+    else
+      items = Item.all
+    end
+    render json: items, include: :user 
   end
+
+  def show
+    user = User.find_by!(id: params[:user_id])
+    if user 
+      item = user.items.find_by(id: params[:id])
+    else
+      return render json: { error: "404 Not Found" }, status: :not_found
+    end
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    if user
+      new_item = Item.create(
+        name: params[:name],
+        description: params[:description],
+        price: params[:price],
+        user_id: params[:user_id]
+      )
+      render json: new_item, status: :created
+    else
+      render json: { error: "404 Not Found" }, status: :not_found
+    end
+  end
+
+
+
+
 
 end
